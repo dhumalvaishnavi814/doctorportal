c49840f95270b185eac35f737a2725ac1ecb204c
dhumalvaishnavi814 <138699577+dhumalvaishnavi814@users.noreply.github.com>
2023-11-03 19:02:55 +0530

    Update message.php

--- message.php
+++ message.php
@@ -9,12 +9,12 @@ $text = mysqli_real_escape_string($conn,$_POST['text']);
 $check_data = "SELECT * FROM chatbot_questionaries where Questions='$text'";
 
 
-$run_query=mysqli_query($conn,$check_data);
+$run_query=mysqli_query($con,$check_data);
 if(mysqli_num_rows($run_query) > 0){
    
     while($row=mysqli_fetch_assoc($run_query)){
 
-    $replay = $row['Answers'];
+    $replay = $row['answers'];
     echo $replay;
     }
 }else{
