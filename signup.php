78949a4469dd19322e7bbe0f7be3dbfc50b38865
dhumalvaishnavi814 <138699577+dhumalvaishnavi814@users.noreply.github.com>
2023-11-04 11:40:16 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,32 +1,32 @@
 
 <?php
-include("conn.php");
-if(isset($_POST["signup"]))
-{
-    // $customer_id=$_POST["customer_id"];
-    $first_name=$_POST["first_name"];
-    $middle_name=$_POST["middle_name"];
-    $last_name=$_POST["last_name"];
-    $dob=$_POST["dob"];
-    $gender=$_POST["gender"];
-    $city=$_POST["city"];
-    $address=$_POST["address"];
-    $mono=$_POST["mono"];
-    $past_records=$_POST["past_records"];
-    $current_health=$_POST["current_health"];
-    $password=$_POST["password"];
-    $email=$_POST["email"];
+// include("conn.php");
+// if(isset($_POST["signup"]))
+// {
+//     // $customer_id=$_POST["customer_id"];
+//     $first_name=$_POST["first_name"];
+//     $middle_name=$_POST["middle_name"];
+//     $last_name=$_POST["last_name"];
+//     $dob=$_POST["dob"];
+//     $gender=$_POST["gender"];
+//     $city=$_POST["city"];
+//     $address=$_POST["address"];
+//     $mono=$_POST["mono"];
+//     $past_records=$_POST["past_records"];
+//     $current_health=$_POST["current_health"];
+//     $password=$_POST["password"];
+//     $email=$_POST["email"];
    
-    $sql  = "INSERT INTO `customer`( `customer_id`,`first_name`, `middle_name`, `last_name`, `dob`, `gender`, `city`, `address`, `mono`, `past_records`, `current_health`,`password`,`email`) VALUES ('','$first_name','$middle_name','$last_name','$dob','$mono','$gender','$city','$address','$past_records','$current_health','$password','$email')";
+//     $sql  = "INSERT INTO `customer`( `customer_id`,`first_name`, `middle_name`, `last_name`, `dob`, `gender`, `city`, `address`, `mono`, `past_records`, `current_health`,`password`,`email`) VALUES ('','$first_name','$middle_name','$last_name','$dob','$mono','$gender','$city','$address','$past_records','$current_health','$password','$email')";
     
-     echo"<br>".$sql;
-if (mysqli_query($con, $sql)) {
-  echo "User created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($con);
-}
+//      echo"<br>".$sql;
+// if (mysqli_query($con, $sql)) {
+//   echo "User created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . mysqli_error($con);
+// }
 
-}
+// }
 ?>
 <html>
 <head>
