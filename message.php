072e77b2eada9e1c0de3f6d5d65e3f7bced60261
dhumalvaishnavi814 <138699577+dhumalvaishnavi814@users.noreply.github.com>
2023-11-04 14:13:25 +0530

    Update message.php

--- message.php
+++ message.php
@@ -7,8 +7,8 @@ $text = mysqli_real_escape_string($con,$_POST['text']);
 
 
 $check_data = "SELECT * FROM chatbot_questionaries where Questions='$text'";
-// echo $check_data;
-
+ echo $check_data;
+echo "$text";
 $run_query=mysqli_query($con,$check_data);
 if(mysqli_num_rows($run_query) > 0){
    
