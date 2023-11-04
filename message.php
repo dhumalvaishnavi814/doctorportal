1f057c742ec4f4fb234fb6da19d46f7fe217850b
dhumalvaishnavi814 <138699577+dhumalvaishnavi814@users.noreply.github.com>
2023-11-04 14:22:21 +0530

    Update message.php

--- message.php
+++ message.php
@@ -24,7 +24,7 @@ if(mysqli_num_rows($run_query) > 0){
    
     while($row=mysqli_fetch_assoc($run_query)){
 
-    $replay = $row['Aswers'];
+    $replay = $row['aswers'];
     echo $replay;
     }
 }else{
